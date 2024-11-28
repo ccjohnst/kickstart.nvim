a18df23ab42f9698b32ed170f3987697246895eb
Chris Johnston <chris@pop-os.Home>
2024-11-28 21:46:40 +0000

    Latest amends

--- init.lua
+++ init.lua
@@ -845,8 +845,9 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'rose-pine-moon'
-
+      -- vim.cmd.colorscheme 'rose-pine-moon'
+      vim.o.background = 'dark' -- or "light" for light mode
+      vim.cmd [[colorscheme gruvbox]]
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
@@ -929,10 +930,10 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,10 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-  'rose-pine/neovim',
-  name = 'rose-pine',
+  {
+
+    'rose-pine/neovim',
+    name = 'rose-pine',
+  },
+  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
 }
